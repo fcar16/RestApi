452470daae3e17b708b1b659b46bc6d054782b7a
fcar1668 <93264300+fcar1668@users.noreply.github.com>
2024-03-05 19:59:20 +0100

    Terminado

--- obj/Debug/net8.0/CajeroAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/CajeroAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CajeroAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce7ea19310168b75be9f057ff0ddad5764df4b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017195ee75115cce553ae0554e00143198c02b88")]
 [assembly: System.Reflection.AssemblyProductAttribute("CajeroAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CajeroAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
